10ab2b6fed605f0aad077741a59e0f28101f4822
JohanVerne <johan.verne2003@gmail.com>
2025-01-05 16:45:31 +0100

    Finish

--- ProjetJava/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.projetjava {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.projetjava to javafx.fxml;
-    exports org.example.projetjava;
-}
